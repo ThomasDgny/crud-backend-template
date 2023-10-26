cf37e765ace6f989162b6199395b0dac4d81f1ad
GoodGuyForReal <tdoganay325@gmail.com>
2023-10-26 22:54:15 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+import express from "express";
+import bodyParser from "body-parser";
+
+const app = express();
+const PORT = 5000;
+
+app.use(bodyParser.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello From server");
+});
+
+app.listen(PORT, () => {
+  console.log(`app is running on http://localhost:${PORT}`);
+});
