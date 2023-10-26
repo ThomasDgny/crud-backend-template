21986786a8e044f63631c51e8bbf2d496b434b47
GoodGuyForReal <tdoganay325@gmail.com>
2023-10-27 00:52:13 +0200

    router added

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,25 @@
 import express from "express";
+import { users } from "../mock-users.js";
+import { v4 as uuidv4 } from "uuid";
 
 const router = express.Router();
 
-router.get("/users", (req, res) => {
-    res.send("Hello From server");
-  });
+router.get("/", (req, res) => {
+  console.log(users);
+  res.send(users);
+});
+
+router.post("/", (req, res) => {
+  const newUser = req.body;
+  users.push({ ...newUser, id: uuidv4() });
+  res.send(`user ${newUser.username} added to database`);
+});
+
+router.get("/:id", (req, res) => {
+  const paramID = req.params.id;
+  const getUser = users.find((user) => user.id === paramID);
+  console.log(getUser)
+  res.send(getUser);
+});
+
+export default router;
