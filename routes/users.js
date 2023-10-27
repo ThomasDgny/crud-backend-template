3c75d375d4e88d31cbaae4351c09835a30a8a11d
GoodGuyForReal <tdoganay325@gmail.com>
2023-10-27 22:54:37 +0200

    update user api route added

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { users } from "../mock-users.js";
+// import { users } from "../mock-users.js";
 import { v4 as uuidv4 } from "uuid";
 
 const router = express.Router();
+let users = [];
 
 router.get("/", (req, res) => {
   res.send(users);
@@ -26,12 +27,26 @@ router.get("/:id", (req, res) => {
 
 router.delete("/:id", (req, res) => {
   const paramID = req.params.id;
-  const getUser = users.filter((user) => user.id !== paramID);
-  if (getUser) {
-    return res.send(getUser);
-  } else {
-    throw new Error("User could not found");
+  users = users.filter((user) => user.id !== paramID);
+  return res.send(`user ${paramID} removed from database`);
+});
+
+router.patch("/:id", (req, res) => {
+  const paramID = req.params.id;
+  const user = users.find((user) => user.id === paramID);
+  if (!user) {
+    return res.status(404).send("User not found");
   }
+  
+  user.username = req.body.username;
+  user.email = req.body.email;
+  user.age = req.body.age;
+
+  console.log(
+    `username has been updated to ${req.body.username}.age has been updated to ${req.body.age}`
+  );
+
+  return res.send(`User with ID ${paramID} updated`);
 });
 
 export default router;
