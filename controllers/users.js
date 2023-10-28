88af27b88bfb99b76fd1645400cfaf00f9707df9
GoodGuyForReal <tdoganay325@gmail.com>
2023-10-28 13:16:46 +0200

    dirty code cleaned from routes and server functions refactored

---
+++ controllers/users.js
@@ -0,0 +1,92 @@
+import { v4 as uuidv4 } from "uuid";
+
+let users = [];
+
+export function getAllUsers(req, res) {
+  res.send(users);
+}
+
+export function createUser(req, res) {
+  try {
+    const newUserBody = req.body;
+    if (!newUserBody.username || !newUserBody.email || !newUserBody.age) {
+      return res.status(400).send({
+        message: "Send all required fields: name, email, age",
+      });
+    }
+
+    const newUser = {
+      username: newUserBody.username,
+      email: newUserBody.email,
+      age: newUserBody.age,
+    };
+
+    users.push({ ...newUser, id: uuidv4() });
+    res.send(`user ${newUser.username} added to database`);
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).send({ message: error.message });
+  }
+}
+
+export function getUser(req, res) {
+  try {
+    const paramID = req.params.id;
+    const getUser = users.find((user) => user.id === paramID);
+    if (!getUser) {
+      return res.status(400).send({
+        message: "User not found",
+      });
+    }
+    return res.send(getUser);
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).send({ message: error.message });
+  }
+}
+
+export function deleteUser(req, res) {
+  try {
+    const paramID = req.params.id;
+    const getUser = users.find((user) => user.id === paramID);
+    if (!getUser) {
+      return res.status(400).send({
+        message: "User not found",
+      });
+    }
+    users = users.filter((user) => user.id !== getUser.id);
+    return res.send(`user ${paramID} removed from database`);
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).send({ message: error.message });
+  }
+}
+
+export function updateUser(req, res) {
+  try {
+    const paramID = req.params.id;
+    const user = users.find((user) => user.id === paramID);
+    if (!user) {
+      return res.status(404).send({
+        message: "User not found",
+      });
+    }
+
+    const { username, email, age } = req.body;
+    
+    if (!username || !email || !age) {
+      return res.status(400).send({
+        message: "Send all required fields: username, email, age",
+      });
+    }
+
+    user.username = username;
+    user.email = email;
+    user.age = age;
+    
+    return res.send(`User with ID ${paramID} updated`);
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).send({ message: error.message });
+  }
+}
--- routes/users.js
+++ routes/users.js
@@ -1,52 +1,22 @@
 import express from "express";
-// import { users } from "../mock-users.js";
-import { v4 as uuidv4 } from "uuid";
+import {
+  createUser,
+  deleteUser,
+  getAllUsers,
+  getUser,
+  updateUser,
+} from "../controllers/users.js";
 
 const router = express.Router();
-let users = [];
 
-router.get("/", (req, res) => {
-  res.send(users);
-});
+router.get("/", (req, res) => getAllUsers(req, res));
 
-router.post("/", (req, res) => {
-  const newUser = req.body;
-  users.push({ ...newUser, id: uuidv4() });
-  res.send(`user ${newUser.username} added to database`);
-});
+router.post("/", (req, res) => createUser(req, res));
 
-router.get("/:id", (req, res) => {
-  const paramID = req.params.id;
-  const getUser = users.find((user) => user.id === paramID);
-  if (getUser) {
-    return res.send(getUser);
-  } else {
-    throw new Error("User could not found");
-  }
-});
+router.get("/:id", (req, res) => getUser(req, res));
 
-router.delete("/:id", (req, res) => {
-  const paramID = req.params.id;
-  users = users.filter((user) => user.id !== paramID);
-  return res.send(`user ${paramID} removed from database`);
-});
+router.delete("/:id", (req, res) => deleteUser(req, res));
 
-router.patch("/:id", (req, res) => {
-  const paramID = req.params.id;
-  const user = users.find((user) => user.id === paramID);
-  if (!user) {
-    return res.status(404).send("User not found");
-  }
-  
-  user.username = req.body.username;
-  user.email = req.body.email;
-  user.age = req.body.age;
-
-  console.log(
-    `username has been updated to ${req.body.username}.age has been updated to ${req.body.age}`
-  );
-
-  return res.send(`User with ID ${paramID} updated`);
-});
+router.patch("/:id", (req, res) => updateUser(req, res));
 
 export default router;
